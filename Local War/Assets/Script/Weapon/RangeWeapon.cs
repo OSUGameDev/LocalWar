d66708f67964d5a1a31a3e960d8fca302c249f5d
Michael Wang <wangshiyao96@gmail.com>
2019-10-16 16:09:33 -0700

    Ver 0.0.0.1
    Create the basic system for Chracter, based on the UML design.

---
+++ Local War/Assets/Script/Weapon/RangeWeapon.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeWeapon : Weapon{
+
+    private int     ammo;
+    private int     magzine;
+    private int     totalAmmo;
+    private float   coolDown;
+    private float   coolDownCounter;
+    private bool    isShooting;
+
+    public GameObject ammoType;
+    public GameObject firePoint;
+
+    public void Fire()
+    {
+        //Cast a ray from center of the camera
+        Ray ray = playerCame.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        //Find the target point
+        RaycastHit hit;
+        //If the ray hit something
+        if (Physics.Raycast(ray, out hit) && !isShooting)
+        {
+            GameObject bullet = Instantiate(ammoType, firePoint.transform.position, firePoint.transform.rotation);
+            LaserAmmo script = bullet.GetComponent<LaserAmmo>();
+            script.setOrigin(firePoint.transform.position);
+            script.initialize(hit.point);
+            isShooting = true;
+            coolDownCounter = coolDown;
+        }
+    }
+    
+	// Use this for initialization
+	void Start () {
+        coolDown = 0.2f;
+        isShooting = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(isShooting)
+        {
+            coolDownCounter -= Time.fixedDeltaTime;
+            if (coolDownCounter <= 0)
+            {
+                isShooting = false;
+            }
+        }     
+	}
+}
