d0a6d4fc073ad8f58a9388bbe64de976ddadfaa1
hodgerti <hodgert@oregonstate.edu>
2019-10-30 18:43:21 -0700

    Built movement engine. Needs to be debugged but the bulk of the code is made.

--- Local War/Assets/Resources/Script/Character/MovementSys.cs
+++ Local War/Assets/Resources/Script/Character/MovementSys.cs
@@ -3,82 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class MovementSys : NetworkBehaviour {
+public class MovementSys : NetworkBehaviour
+{
+    private Vector3 player_velocity;
+    private Vector3 player_acceleration;
+    public Vector3 min_speed = new Vector3(10.0f, 0.0f, 10.0f);
+    public Vector3 max_speed = new Vector3(600.0f, 0.0f, 600.0f);
+    public Vector3 acceleration = new Vector3(50.0f, 0.0f, 50.0f);
+    public Vector3 decceleration = new Vector3(50.0f, 0.0f, 50.0f);
+    private Vector2 previous_input_direction;
+    private Vector2 input_direction;
+    private Vector2 previous_mouse_axis;
+    private Vector2 mouse_axis;
+    public float sensitivity = 100.0f;
 
-    private float speed = 2.0F;          //the moving speed of the character
-    private float jumpSpeed = 10.0f;      //the jump force of the character
-    private float rotateSpeed = 100.0f;
+    private CharacterController kinematic_controller;
 
-    private float gravity = 20.0f;       //the force of gravity on the character
+    private int jumps_used = 0;
+    public int jump_count = 2;
+    public float gravity = 9.8f;
+    public float jump_speed = 10.0f;
 
-    private float groundOffset = .2f;    //the offset for the IsGrounded check. Useful for recognizing slopes and imperfect ground.
-
-    private float moveH, moveV;
-    private float mouseX, mouseY;
-    private Vector3 moveDirection = Vector3.zero;   //the direction the character should move.
-    private Vector3 jumpDirection = Vector3.zero;
-    private Quaternion rotationStart, rotationEnd;
-
-    private CharacterController controller;
-
-
-    ///The check to see if the character is currently on the ground.
-    private bool isGrounded()
-    {
-        RaycastHit hit;
-        Physics.Raycast(this.transform.position, -this.transform.up, out hit, 10);   //A short ray shot directly downward from the center of the character.
-
-        if (System.Math.Abs(hit.distance) < System.Single.Epsilon)                                           //if the distance is zero, the ray probably did not hit anything.
-        {
-            return false;
-        }
-        if (hit.distance <= (this.transform.lossyScale.y / 2 + groundOffset))   //if the distance from the ray is less than half the height 
-        {                                                                   //of the character (plus the offset), the character us grounded.
-            return true;
-        }
-        return false;
-    }
-
-    private void move()
-    {
-        /*****Get basic player input*****/
-        moveH = Input.GetAxis("Horizontal");
-        moveV = Input.GetAxis("Vertical");
-        mouseX += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
-        mouseY -= Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
-        mouseY = Mathf.Clamp(mouseY, -90, 90);
-
-        rotationStart = transform.rotation;
-        rotationEnd = Quaternion.Euler(mouseY, mouseX, 0f);
-        transform.rotation = Quaternion.Lerp(rotationStart, rotationEnd, 0.5f);
-
-        moveDirection = new Vector3(moveH, 0.0f, moveV);       //Create the player's movement from keyboard in local space
-        moveDirection = transform.TransformDirection(moveDirection);      //Transform the moveMent from local space to world space
-        moveDirection *= speed;      //Based on base speed
-
-        /*****Check jump mode at last*****/
-        if (Input.GetButtonDown("Jump"))               //jump if the character is grounded and the user presses the jump button.
-        {
-            jumpDirection.y = jumpSpeed;     //Give a jump speed to player
-        }
-
-
-        /*****Move the player*****/
-        controller.Move(moveDirection * Time.deltaTime);    //move the character based on the gravitational force.
-        if (!controller.isGrounded)
-        {
-            jumpDirection.y -= gravity * Time.deltaTime;
-        }
-        controller.Move(jumpDirection * Time.deltaTime);
-    }
-
-    //This built-in function will be called after the script first time loaded into the scene
     void Start()
     {
-        mouseX = 0;
-        mouseY = 0;
+        player_velocity = min_speed;
+        player_acceleration = new Vector3();
+        input_direction = new Vector2();
+        mouse_axis = new Vector2();
 
-        controller = GetComponent<CharacterController>();
+        kinematic_controller = GetComponent<CharacterController>();
         transform.Find("Main Camera").GetComponent<Camera>().enabled = hasAuthority;
         if (hasAuthority)
         {
@@ -87,16 +40,129 @@ public class MovementSys : NetworkBehaviour {
         }
     }
 
-    void Update()
+    private void GetInput()
+    {
+        // states
+        previous_input_direction = input_direction;
+        previous_mouse_axis = mouse_axis;
+
+        // looking 
+        // TODO: break modifying mouse movement from getting input
+        mouse_axis.x += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouse_axis.y -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        mouse_axis.y = Mathf.Clamp(mouse_axis.y, -89.999f, 89.999f);
+        Quaternion rotation_start = transform.rotation;
+        Quaternion rotation_end = Quaternion.Euler(mouse_axis.y, mouse_axis.x, 0.0f);
+        transform.rotation = Quaternion.Lerp(rotation_start, rotation_end, 0.5f);
+        // TODO: add forward facing vector
+
+        // movement
+        input_direction.x = Mathf.Sign(Input.GetAxis("Horizontal"));
+        input_direction.y = Mathf.Sign(Input.GetAxis("Vertical"));
+    }
+
+    private void Move()
+    {
+
+        // jumping:
+        if (Input.GetButtonDown("Jump"))
+        {
+            player_velocity.y += jump_speed;
+        }
+
+        // movement:
+
+        // get acceleration
+        player_acceleration.x = 0.0f;
+        player_acceleration.y = 0.0f; // -gravity;
+        player_acceleration.z = 0.0f;
+        bool stop_strafe = true;
+        bool stop_walk = true;
+
+        // left
+        if (input_direction.x < 0)
+        {
+            if (player_velocity.x <= min_speed.x && player_velocity.x > -max_speed.x)
+            {
+                player_acceleration.x = acceleration.x * input_direction.x;
+                stop_strafe = false;
+            }
+        }
+        // right
+        else if (input_direction.x > 0)
+        {
+            if (player_velocity.x >= -min_speed.x && player_velocity.x < max_speed.x)
+            {
+                player_acceleration.x = acceleration.x * input_direction.x;
+                stop_strafe = false;
+            }
+        }
+        // forward
+        if (input_direction.y < 0)
+        {
+            if (player_velocity.z <= min_speed.z && player_velocity.z > -max_speed.z)
+            {
+                player_acceleration.z = acceleration.z * input_direction.y;
+                stop_walk = false;
+            }
+        }
+        // back
+        else if (input_direction.y > 0)
+        {
+            if (player_velocity.z >= -min_speed.z && player_velocity.z < max_speed.z)
+            {
+                player_acceleration.z = acceleration.z * input_direction.y;
+                stop_walk = false;
+            }
+        }
+
+        // stop strafe
+        if (stop_strafe)
+        {
+            float v_sign = Mathf.Sign(player_velocity.x);
+            float v_len = Mathf.Abs(player_velocity.x) - (decceleration.x * Time.deltaTime);
+            if (v_len < 0)
+            {
+                v_len = 0;
+            }
+            player_velocity.x = v_sign * v_len;
+        }
+        // stop walk
+        if (stop_walk)
+        {
+            float v_sign = Mathf.Sign(player_velocity.z);
+            float v_len = Mathf.Abs(player_velocity.z) - (decceleration.z * Time.deltaTime);
+            if (v_len < 0)
+            {
+                v_len = 0;
+            }
+            player_velocity.z = v_sign * v_len;
+        }
+
+        // apply acceleration
+        player_velocity.x += player_acceleration.x * Time.deltaTime;
+        player_velocity.y += player_acceleration.y * Time.deltaTime;
+        player_velocity.z += player_acceleration.z * Time.deltaTime;
+
+        // apply movement
+        Debug.Log( player_velocity );
+        kinematic_controller.Move(player_velocity);
+    }
+
+    void FixedUpdate()
     {
         if (!hasAuthority)
             return;
-        move();
-        if(Input.GetKeyDown(KeyCode.Escape))
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             NetworkManager.singleton.StopClient();
         }
+
+        GetInput();
+        Move();
     }
+
 }
