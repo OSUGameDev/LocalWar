28e0bc219ef3fb3a83a444e0c6b36b4241cd251e
Carson P <PembleC@oregonstate.edu>
2019-11-07 11:28:50 -0800

    Fire Gun Sound
    This allows the client to hear a gun shot sound (currently all are just laser guns)

---
+++ Local War/Assets/Resources/Script/System/SoundSys.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundSys {
+
+
+    public static void PlaySound(AudioClip sound)
+    {
+        float lowVol = 0.25f;
+        float highVol = 1.25f;
+
+
+        Debug.Log("Called the PlaySound in SoundSys");
+
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+
+        audioSource.PlayOneShot(sound);
+
+
+    }
+
+  
+}
