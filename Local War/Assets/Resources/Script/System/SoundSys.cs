b3ba5af57c33fe2088b3f9da2765fc493c745ee3
Carson P <PembleC@oregonstate.edu>
2019-11-09 14:06:43 -0800

    Gun Volume Change
    Now every shot doesn't sound the same. Just an annoying bug that needed to be fixed.

--- Local War/Assets/Resources/Script/System/SoundSys.cs
+++ Local War/Assets/Resources/Script/System/SoundSys.cs
@@ -8,7 +8,7 @@ public static class SoundSys {
     public static void PlaySound(AudioClip sound)
     {
         float lowVol = 0.25f;
-        float highVol = 1.25f;
+        float highVol = 1.0f;
 
 
         Debug.Log("Called the PlaySound in SoundSys");
@@ -16,8 +16,9 @@ public static class SoundSys {
         GameObject soundGameObject = new GameObject("Sound");
         AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
 
-        audioSource.PlayOneShot(sound);
+        float vol = Random.Range(lowVol, highVol);
 
+        audioSource.PlayOneShot(sound, vol);
 
     }
 
