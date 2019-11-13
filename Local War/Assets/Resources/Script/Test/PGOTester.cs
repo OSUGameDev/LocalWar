c37a25093b6e05a5a5ca33c170143abc9c7f0c21
Brendan Moore <brendanmoore98@gmail.com>
2019-11-13 14:39:31 -0800

    Solved id problem, merging with dev to get rid of annoying scene change

---
+++ Local War/Assets/Resources/Script/Test/PGOTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PGOTester : MonoBehaviour {
+
+	public GameObject testObj;
+	protected PGOManager pgoManager;
+
+	// Use this for initialization
+	void Start () {
+		pgoManager = GetComponent<PGOManager>();
+		Debug.Log(pgoManager);
+		Type key = pgoManager.InitObject(testObj);
+
+		Debug.Log("Type:" + key.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1"))
+        {
+            
+
+        }
+	}
+}
