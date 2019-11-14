b251a247102630c8fa57c7be28bcc454386ada8f
hodgerti <hodgerti@oregonstate.edu>
2019-11-13 20:57:58 -0800

    Got working movement system. Added a TODO list to turn it from acceptable to extremely passable.

--- Local War/Assets/Resources/Script/Character/MovementSys.cs
+++ Local War/Assets/Resources/Script/Character/MovementSys.cs
@@ -7,10 +7,9 @@ public class MovementSys : NetworkBehaviour
 {
     private Vector3 player_velocity;
     private Vector3 player_acceleration;
-    public Vector3  min_speed;
-    public Vector3  max_speed;
     public Vector3  acceleration;
-    public Vector3  decceleration;
+    public float    kinetic_friction;
+    public float    air_friction;
     private Vector3 previous_input_direction;
     private Vector3 input_direction;
     private Vector2 previous_mouse_axis;
@@ -21,20 +20,26 @@ public class MovementSys : NetworkBehaviour
 
     private int jumps_used = 0;
     public int jump_count = 2;
-    public float gravity = 9.8f;
-    public float jump_speed = 10.0f;
+    public float gravity = 3.2f;
+    public float jump_speed = 4.0f;
 
+    /* 
+     * TODO:
+     * Turn acceleration into a force. Right now the units don't match whats happening.
+     * Fix jumping.
+     * Enable some way to adjust velocity upon collision
+     * Make variable speeds possible for strafing/moving forward/moving backwards
+    */
     void Start()
     {
-        min_speed       = new Vector3(1.0f, 0.0f, 1.0f);
-        max_speed       = new Vector3(10.0f, 0.0f, 10.0f);
-        acceleration    = new Vector3(40.0f, 0.0f, 40.0f);
-        decceleration   = new Vector3(60.0f, 0.0f, 60.0f);
+        acceleration        = new Vector3(1.8f, 0.0f, 1.8f);
+        kinetic_friction    = 0.35f;
+        air_friction        = 0.01f;
 
-        player_velocity = min_speed;
+        player_velocity     = new Vector3();
         player_acceleration = new Vector3();
-        input_direction = new Vector2();
-        mouse_axis = new Vector2();
+        input_direction     = new Vector2();
+        mouse_axis          = new Vector2();
 
         kinematic_controller = GetComponent<CharacterController>();
         transform.Find("Main Camera").GetComponent<Camera>().enabled = hasAuthority;
@@ -61,9 +66,10 @@ public class MovementSys : NetworkBehaviour
         transform.rotation = Quaternion.Lerp(rotation_start, rotation_end, 0.5f);
 
         // movement
-        input_direction.x = signf(Input.GetAxis("Horizontal"));
+        input_direction.x = Input.GetAxis("Horizontal");
         input_direction.y = 0.0f;
-        input_direction.z = signf(Input.GetAxis("Vertical"));
+        input_direction.z = Input.GetAxis("Vertical");
+        Debug.Log(input_direction);
     }
 
     private void Move()
@@ -77,83 +83,30 @@ public class MovementSys : NetworkBehaviour
 
         // movement:
 
-        // get acceleration
-        player_acceleration.x = 0.0f;
-        player_acceleration.y = 0.0f; // -gravity * Time.deltaTime;
-        player_acceleration.z = 0.0f;
-
         // strafing
         if ( input_direction.x != 0.0f )
         {
-            // max speed constraints
-            if (player_velocity.x < -max_speed.x || player_velocity.x > max_speed.x)
-            {
-                player_velocity.x = max_speed.x * signf(player_velocity.x);
-            }
-            // accelerate left/right in regard to forward
-            else
-            {
-                Vector3 movement_forward = new Vector3(transform.forward.x, 0.0f, transform.forward.z); movement_forward.Normalize();
-                Vector3 movement_cross = Vector3.Cross(movement_forward, Vector3.up);
-                player_acceleration -= movement_cross * acceleration.x * input_direction.x;
-            }
+            Vector3 movement_forward = new Vector3(transform.forward.x, 0.0f, transform.forward.z); movement_forward.Normalize();
+            Vector3 movement_cross = Vector3.Cross(movement_forward, Vector3.up);
+            player_velocity -= movement_cross * acceleration.x * input_direction.x * Time.deltaTime;
         }
 
         // forward/backwards
         if (input_direction.z != 0.0f)
         {
-            // max speed constraints
-            if (player_velocity.z < -max_speed.z || player_velocity.z > max_speed.z)
-            {
-                player_velocity.z = max_speed.z * signf(player_velocity.z);
-            }
-            // accelerate with forward on x,z plane
-            else
-            {
-                Vector3 movement_forward = new Vector3(transform.forward.x, 0.0f, transform.forward.z); movement_forward.Normalize();
-                player_acceleration += movement_forward * acceleration.z * input_direction.z;
-            }
+            Vector3 movement_forward = new Vector3(transform.forward.x, 0.0f, transform.forward.z); movement_forward.Normalize();
+            player_velocity += movement_forward * acceleration.z * input_direction.z * Time.deltaTime;
         }
 
-        // friction
-        if (input_direction.x == 0.0f && input_direction.z == 0.0f)
-        {
-            // static friction
-            if (player_velocity.x > -min_speed.x && player_velocity.x < min_speed.x)
-            {
-                player_velocity.x = 0.0f;
-            }
-            // dynamic friction
-            else
-            {
-                Vector3 velocity_forward = new Vector3(signf(player_velocity.x), 0.0f, signf(player_velocity.z)); velocity_forward.Normalize();
-                Vector3 movement_cross = Vector3.Cross(velocity_forward, Vector3.up);
-                player_acceleration += movement_cross * decceleration.x;
-            }
+        // kinetic friction
+        Vector3 opposite_velocity = new Vector3(-player_velocity.x, 0.0f, -player_velocity.z);
+        player_velocity += opposite_velocity * kinetic_friction;
 
-            // static friction
-            if (player_velocity.z > -min_speed.z && player_velocity.z < min_speed.z)
-            {
-                player_velocity.z = 0.0f;
-            }
-            // dynamic friction
-            else
-            {
-                Vector3 velocity_forward = new Vector3(signf(player_velocity.x), 0.0f, signf(player_velocity.z)); velocity_forward.Normalize();
-                player_acceleration -= velocity_forward * decceleration.z;
-            }
-        }
-
-        // apply acceleration
-        player_velocity.x += player_acceleration.x * Time.deltaTime;
-        player_velocity.y += player_acceleration.y * Time.deltaTime;
-        player_velocity.z += player_acceleration.z * Time.deltaTime;
-
-        Debug.Log(player_acceleration);
+        // gravity
+        player_velocity.y -= gravity * Time.deltaTime;
 
         // apply movement
-        Vector3 translation = player_velocity * Time.deltaTime;
-        transform.Translate(translation, Space.World);
+        kinematic_controller.Move(player_velocity);
     }
 
     void Update()
@@ -167,7 +120,10 @@ public class MovementSys : NetworkBehaviour
             Cursor.visible = true;
             NetworkManager.singleton.StopClient();
         }
+    }
 
+    private void FixedUpdate()
+    {
         GetInput();
         Move();
     }
