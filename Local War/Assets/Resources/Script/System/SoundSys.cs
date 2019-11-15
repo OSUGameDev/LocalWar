5c9c9177362d52b829463cd3eca15d49d810af82
Carson P <PembleC@oregonstate.edu>
2019-11-14 16:28:17 -0800

    Fixed Sound System
     Object Creation only happens if null

--- Local War/Assets/Resources/Script/System/SoundSys.cs
+++ Local War/Assets/Resources/Script/System/SoundSys.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public static class SoundSys {
 
+    private static GameObject oneSoundGameObject;
+    private static AudioSource oneAudioSource;
+
 
     public static void PlaySound(AudioClip sound)
     {
+        //Debug.Log("Called the PlaySound in SoundSys");
+
         float lowVol = 0.25f;
         float highVol = 1.0f;
-
-
-        Debug.Log("Called the PlaySound in SoundSys");
-
-        GameObject soundGameObject = new GameObject("Sound");
-        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
-
         float vol = Random.Range(lowVol, highVol);
 
-        audioSource.PlayOneShot(sound, vol);
+        if (oneSoundGameObject == null)
+        {
+            oneSoundGameObject = new GameObject("Single Sound");
+            oneAudioSource = oneSoundGameObject.AddComponent<AudioSource>();
+        }
+
+        oneAudioSource.PlayOneShot(sound, vol);
 
     }
+
 
   
 }
