7d28ef7d7eaf7b1b2a838650fa9178a161d8fb8d
Trevor Webster <34354663+isvpursuit@users.noreply.github.com>
2020-01-19 13:44:57 -0800

    WFC Prefabs
    5 Basic Prefabs and some preliminary code for the WFC function implemented

---
+++ Local War/Assets/Resources/Script/WFC Script/MapBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class MapBehavior : MonoBehaviour
+{
+    public int MapHeight = 6;
+    public int MapLength = 20;
+    public int MapWidth = 20;
+}
---
+++ Local War/Assets/Resources/Script/WFC Script/ModulePrototype.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class ModulePrototype : MonoBehaviour
+{
+	[System.Serializable]
+	public abstract class FaceDetails
+	{
+		public bool Walkable;
+		public int Connector;
+		public virtual void ResetConnector()
+		{
+			this.Connector = 0;
+		}
+		public ModulePrototype[] ExcludedNeighbours;
+		public bool EnforceWalkableNeighbor = false;
+		public bool IsOcclusionPortal = false;
+	}
+
+	[System.Serializable]
+	public class HorizontalFaceDetails : FaceDetails
+	{
+		public bool Symmetric;
+		public bool Flipped;
+
+		public override string ToString()
+		{
+			return this.Connector.ToString() + (this.Symmetric ? "s" : (this.Flipped ? "F" : ""));
+		}
+
+		public override void ResetConnector()
+		{
+			base.ResetConnector();
+			this.Symmetric = false;
+			this.Flipped = false;
+		}
+	}
+
+	[System.Serializable]
+	public class VerticalFaceDetails : FaceDetails
+	{
+		public bool Invariant;
+		public int Rotation;
+
+		public override string ToString()
+		{
+			return this.Connector.ToString() + (this.Invariant ? "i" : (this.Rotation != 0 ? "_bcd".ElementAt(this.Rotation).ToString() : ""));
+		}
+
+		public override void ResetConnector()
+		{
+			base.ResetConnector();
+			this.Invariant = false;
+			this.Rotation = 0;
+		}
+	}
+
+	public float Probability = 1.0f;
+	public bool Spawn = true;
+	public bool IsInterior = false;
+
+	public HorizontalFaceDetails Left;
+	public VerticalFaceDetails Down;
+	public HorizontalFaceDetails Back;
+	public HorizontalFaceDetails Right;
+	public VerticalFaceDetails Up;
+	public HorizontalFaceDetails Forward;
+
+	public FaceDetails[] Faces
+	{
+		get
+		{
+			return new FaceDetails[] {
+				this.Left,
+				this.Down,
+				this.Back,
+				this.Right,
+				this.Up,
+				this.Forward
+			};
+		}
+	}
+
+	public Mesh GetMesh(bool createEmptyFallbackMesh = true)
+	{
+		var meshFilter = this.GetComponent<MeshFilter>();
+		if (meshFilter != null && meshFilter.sharedMesh != null)
+		{
+			return meshFilter.sharedMesh;
+		}
+		if (createEmptyFallbackMesh)
+		{
+			var mesh = new Mesh();
+			return mesh;
+		}
+		return null;
+	}
+
+	void Update() { }
+
+	void Reset()
+	{
+		this.Up = new VerticalFaceDetails();
+		this.Down = new VerticalFaceDetails();
+		this.Right = new HorizontalFaceDetails();
+		this.Left = new HorizontalFaceDetails();
+		this.Forward = new HorizontalFaceDetails();
+		this.Back = new HorizontalFaceDetails();
+
+		foreach (var face in this.Faces)
+		{
+			face.ExcludedNeighbours = new ModulePrototype[] { };
+		}
+	}
+}
