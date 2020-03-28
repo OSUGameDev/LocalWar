91210cb9b446591814d6bba57eb3a4c672d7e25e
tremperb <55967770+tremperb@users.noreply.github.com>
2020-03-27 18:06:18 -0700

    Added pit and trigger for it when bottom is hit

---
+++ Local War/Assets/handlePit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handlePit : MonoBehaviour
+{
+    private BoxCollider other;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //When the plane has been hit.
+    //This would mean the player should die 
+    //and respawn somewhere.
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("You have hit the pit");
+    }
+}
